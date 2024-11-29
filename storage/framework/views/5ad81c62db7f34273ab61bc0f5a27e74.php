dd5a1a8cec271a3492c8a12d74c6193b1a70bcf1
naaannaaa <132665892+naaannaaa@users.noreply.github.com>
2024-11-29 22:11:19 +0300

    Add files via upload

---
+++ storage/framework/views/5ad81c62db7f34273ab61bc0f5a27e74.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Запросы</title>
+</head>
+<body>
+
+    <div class="container">
+        <h1>Запросы</h1>
+        <button type="button" class="btn btn-success">Редактировать</button>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Название</th>
+                    <th scope="col">Описание</th>
+                    <th scope="col">Статус</th>
+                    <th scope="col">Редактировать</th>
+
+                </tr>
+
+
+            </thead>
+        </table>
+    
+</body>
+</html><?php /**PATH D:\OSPanel\home\testzome.local\public\HelpDesk\resources\views/tickets.blade.php ENDPATH**/ ?>
